ce74500a0c20aa010d9272bca658ac1ccf99f5d9
zhu <1431722804@qq.com>
2023-12-11 09:49:35 +0800

    add sub

--- file01.c
+++ file01.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
+int sub(int a,int b)
+{
+	return a - b;
+ } 
 int main()
 {
-    printf("hello world");
-    printf("Why Why Why"); 
+    printf("hello world\n");
+    printf("Why Why Why\n");
+	printf("%d",sub(1,1)); 
     return 0;
 }
